0365ccc855822ad1444e0162da56cef1ed0abc9b
Jacob Hampton <jacob.hampton@hotmail.co.uk>
2022-02-25 15:37:23 +0000

    removed log4j, will use logback in the future

--- src/main/java/com/jacoobia/bingobookbot/model/security/user/AuthFailureHandler.java
+++ src/main/java/com/jacoobia/bingobookbot/model/security/user/AuthFailureHandler.java
@@ -1,5 +1,7 @@
 package com.jacoobia.bingobookbot.model.security.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,17 @@ import java.io.IOException;
 @Component
 public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthFailureHandler.class);
+
+    private static final String AUTH_DENIED = "[access_denied] The resource owner or authorization server denied the request";
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        exception.printStackTrace();
-        System.out.println("oh no... out oauth2.... it's broken!");
+        if(!AUTH_DENIED.equals(exception.getMessage()))
+        {
+            final String message = "Exception occurred when trying to authorize a discord login request.";
+            logger.error(message, exception);
+        }
     }
 
 }
